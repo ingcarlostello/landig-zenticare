5f59b50c41153ad597c337576bdd5238dd18e62b
Carlos Andres <carlos@MacBook-Pro-de-Carlos.local>
2026-01-30 19:45:56 -0500

    elimianr codigo no usado

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import HowItWorks from './components/HowItWorks'
-import Testimonials from './components/Testimonials'
-import Pricing from './components/Pricing'
 import Footer from './components/Footer'
 
 function App() {
